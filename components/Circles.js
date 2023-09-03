66b3c6a62e0f53a4527879c90cc1be6fdb6e5069
issamssm <lizameissam16@gmail.com>
2023-09-03 23:19:45 +0200

    first commit

---
+++ components/Circles.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+
+const Circles = () => {
+  return (
+    <div className=" w-[200px] animate-pulse duration-75 z-10 xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge" >
+      <Image
+        src={'/circles.png'}
+        width={260}
+        height={200}
+        alt=""
+        className=" w-full h-full" />
+    </div>
+  )
+};
+
+export default Circles;
